4332cc1e2d47d590e44518b2c49d92048b1aaf04
jose fraga <josemendezfraga@gmail.com>
2021-07-23 16:55:09 -0400

    redirecting when user logs out

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,11 +1,13 @@
 import { useState, useContext, useEffect } from 'react';
 import TheContext from '../TheContext';
 import actions from '../api';
+import { useHistory } from 'react-router-dom';
 
 
 function Profile(props) {
     const { user, setUser } = useContext(TheContext)
     const [userUpdated, setUserUpdated] = useState('')
+    const history = useHistory();
 
     useEffect( () => {
         (async () => {            
@@ -17,13 +19,15 @@ function Profile(props) {
     const logOut = () => {
         localStorage.removeItem('token')
         setUser(null)
+        history.push('/Auth')
     }
 
     console.log(user);
+
     return (
         <div>
-            <h2>Profile {props.user?.name}</h2>
-            <img src={user?.imageUrl} />
+            <h2>Profile {userUpdated.name}</h2>
+            <img src={userUpdated.imageUrl} />
             <h2>country: {userUpdated.country}</h2>
             <button onClick={logOut}>Log out</button>
         </div>
