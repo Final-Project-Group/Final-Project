dd0d1e5c3b03f7e84c69fc6ca77e76dd7f54b326
Mario P. Cardenas <15961362+Mario-paul@users.noreply.github.com>
2021-07-28 17:08:16 -0400

    navbar title almost fixed

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,30 +1,32 @@
-import React from 'react';
-import {Link, Route} from 'react-router-dom'
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import React, { useContext } from "react";
+import { Link, Route } from "react-router-dom";
+import clsx from "clsx";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import Drawer from "@material-ui/core/Drawer";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import List from "@material-ui/core/List";
+import Typography from "@material-ui/core/Typography";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
 import "../App.css";
+import TheContext from "../TheContext";
+import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+import AddRoundedIcon from "@material-ui/icons/AddRounded";
 
-import { useHistory ,useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from "react-router-dom";
 
 // const location = useLocation()
 // location.pathname
-
 
 // import { createTheme } from '@material-ui/core/styles';
 // import purple from '@material-ui/core/colors/purple';
@@ -44,10 +46,10 @@ const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
+    display: "flex",
   },
   appBar: {
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
@@ -55,7 +57,7 @@ const useStyles = makeStyles((theme) => ({
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
-    transition: theme.transitions.create(['margin', 'width'], {
+    transition: theme.transitions.create(["margin", "width"], {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -64,7 +66,7 @@ const useStyles = makeStyles((theme) => ({
     marginRight: theme.spacing(2),
   },
   hide: {
-    display: 'none',
+    display: "none",
   },
   drawer: {
     width: drawerWidth,
@@ -72,51 +74,66 @@ const useStyles = makeStyles((theme) => ({
   },
   drawerPaper: {
     width: drawerWidth,
+    background: "#161616",
   },
   drawerHeader: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     padding: theme.spacing(0, 1),
     // necessary for content to be below app bar
     ...theme.mixins.toolbar,
-    justifyContent: 'flex-end',
+    justifyContent: "flex-end",
   },
   content: {
     flexGrow: 1,
     padding: theme.spacing(3),
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.leavingScreen,
     }),
     marginLeft: -drawerWidth,
   },
   contentShift: {
-    transition: theme.transitions.create('margin', {
+    transition: theme.transitions.create("margin", {
       easing: theme.transitions.easing.easeOut,
       duration: theme.transitions.duration.enteringScreen,
     }),
     marginLeft: 0,
   },
-  
+
   // offset: theme.mixins.toolbar,
 }));
 
 export default function Navbar(props) {
-  const classes = useStyles();
+  const classes = useStyles(); // MaterialUI use styles
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
 
+  // Print pathname on to navbar title
+  const history = useHistory();
 
-  
-  const history = useHistory()
   const returnPathname = () => {
-    console.log(history.location.pathname)
-    
-  }
+    console.log(history.location.pathname);
+    let pathname = history.location.pathname;
+    pathname = pathname.slice(1, pathname.length);
+    pathname = pathname.charAt(0).toUpperCase() + pathname.slice(1);
+    console.log(pathname);
+    console.log(pathname.substr(0, pathname.lastIndexOf("/")));
+    let includesNum = false;
+    pathname.split("").map((eachChar) => {
+      if (!isNaN(eachChar)) {
+        includesNum = true;
+      }
+    });
+    console.log(includesNum);
+    return includesNum
+      ? pathname.substr(0, pathname.lastIndexOf("/"))
+      : pathname;
+  };
 
+  const { user } = useContext(TheContext);
 
-  console.log(props)
-
+  console.log(user);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -133,7 +150,6 @@ export default function Navbar(props) {
         position="fixed"
         className={clsx(classes.appBar, {
           [classes.appBarShift]: open,
-          
         })}
         // style={{background: 'transparent', boxShadow: 'none'}}
       >
@@ -148,11 +164,11 @@ export default function Navbar(props) {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" noWrap>
-          home
+            {returnPathname()}
           </Typography>
         </Toolbar>
       </AppBar>
-      
+
       <Drawer
         className={classes.drawer}
         variant="persistent"
@@ -161,31 +177,65 @@ export default function Navbar(props) {
         classes={{
           paper: classes.drawerPaper,
         }}
-        
       >
         <div className={classes.drawerHeader}>
           <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+            {theme.direction === "ltr" ? (
+              <ChevronLeftIcon />
+            ) : (
+              <ChevronRightIcon />
+            )}
           </IconButton>
         </div>
         <Divider />
-        <List onClick={() => {handleDrawerClose()}}>
-          {[<Link to="/home" >Home</Link>, <Link to="/CreateEvent">Create Event</Link>, <Link to="/Profile">Profile</Link>].map((text, index) => (
+        <List
+          onClick={() => {
+            handleDrawerClose();
+          }}
+        >
+          {[
+            <Link
+              to="/Profile"
+              style={{ textDecoration: "none", color: "lightgray" }}
+            >
+              Profile
+            </Link>,
+            <Link
+              to="/home"
+              style={{ textDecoration: "none", color: "lightgray" }}
+            >
+              Home
+            </Link>,
+            <Link
+              to="/CreateEvent"
+              style={{ textDecoration: "none", color: "lightgray" }}
+            >
+              Create Event
+            </Link>,
+          ].map((text, index) => (
             <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+              <ListItemIcon>
+                {index === 0 ? (
+                  <img src={user.imageUrl} className="navProfileImg" />
+                ) : index === 1 ? (
+                  <HomeRoundedIcon style={{ fill: "lightgray" }} />
+                ) : (
+                  <AddRoundedIcon style={{ fill: "lightgray" }} />
+                )}
+              </ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
         </List>
         <Divider />
-        <List>
+        {/* <List>
           {['All mail', 'Trash', 'Spam'].map((text, index) => (
             <ListItem button key={text}>
               <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
               <ListItemText primary={text} />
             </ListItem>
           ))}
-        </List>
+        </List> */}
       </Drawer>
       {/* <main
         className={clsx(classes.content, {
