6bf13780d30d4aada86501a90bdfc815a2c8ec90
jose fraga <josemendezfraga@gmail.com>
2021-08-01 13:58:52 -0400

    cards are all the same height and responsive (home page)

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -87,12 +87,12 @@ function Home(props) {
                                             </div>
                                         </Typography>
                                         <Typography className="home-event-info-1" color='white' style={{ background: 'rgb(75,105,40)'}}>
-                                            <div><span style={{color:'white'}} >{event.location}</span></div>
+                                            <div><span style={{color:'white'}} >{event.location.substring(0,20)}</span></div>
                                             <span className="home-event-span" style={{color:'white'}} > {event.date.split('T', 1)}</span>
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
-                                <Typography className="home-event-info" color="textPrimary" style={{ backgroundImage: 'linear-gradient(rgb(75,105,40), black)'}}>
+                                <Typography className="home-event-info" style={{ backgroundImage: 'linear-gradient(rgb(75,105,40), black)'}}>
                                     <div className="home-event-info-row">
                                         <span lassName="home-event-span" style={{color:'white'}} >Creator  <br/>  {event.creator.name}</span>
                                         <span style={{color:'white'}} >Spots   <br/> {event.spots - event.members.length}/{event.spots}</span>
@@ -117,7 +117,6 @@ function Home(props) {
                                 id="card" 
                                 style={{
                                     borderRadius: '5%',
-                                    // border: '6px solid rgb(13,92,30)'
                                 }}
                             >
                                 <CardActionArea>
@@ -128,14 +127,14 @@ function Home(props) {
                                     image={event.image}
                                     title="Sport Image"
                                     />
-                                    <CardContent style={{backgroundColor: 'rgb(75,105,40)', color: 'white'}}>
+                                    <CardContent style={{backgroundColor: 'rgb(75,105,40)', color: 'white', height: '20vh'}}>
                                         <Typography className="home-event-card" gutterBottom variant="h5" component="h2" color="textPrimary" style={{fontFamily: 'Roboto', backgroundColor: 'rgb(75,105,40)'}}>
                                             <div className="home-event-typography">
                                                 <h4 style={{color:'white'}} >{event.eventName} {event.sport === 'soccer' ? <SportsSoccerIcon/> : event.sport === 'basketball' ? <SportsBasketballIcon/> :  <SportsTennisIcon/>}</h4>
                                             </div>
                                         </Typography>
                                         <Typography className="home-event-info-1" color="textPrimary" style={{ background: 'rgb(75,105,40)'}}>
-                                            <div><span style={{color:'white'}} >{event.location}</span></div>
+                                            <div ><span style={{color:'white'}}>{event.location.substring(0,20)}</span></div>
                                             <span className="home-event-span" style={{color:'white'}} > {event.date.split('T', 1)}</span>
                                         </Typography>
                                     </CardContent>
